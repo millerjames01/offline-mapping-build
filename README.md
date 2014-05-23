962fea6db9edd5e03ccb2f973bbf62734035d3e9
Jim Miller <miller.james01@gmail.com>
2014-05-23 13:24:06 -0400

    putting the finishing touches on, cutting out excess

--- README.md
+++ README.md
@@ -7,15 +7,16 @@ are compiled and packaged by Phonegap Build
 
 Requirements
 ------------
-Having the [Android SDK](https://developer.android.com/sdk/index.html) is also necessary for 
-using phone emulators, installing the app, and 
+Having the [Android SDK](https://developer.android.com/sdk/index.html) 
+is also necessary for using phone emulators, installing the app, and 
 viewing the log.
 
 
 Demo
 ----
-Go to the app's [page](https://build.phonegap.com/apps/878115/builds "Offline Mapping") on Phonegap Build
-and download the .apk (Windows Phone and iPhone apps
+Go to the app's 
+[page](https://build.phonegap.com/apps/878115/builds "Offline Mapping") 
+on Phonegap Build and download the .apk (Windows Phone and iPhone apps
 are not yet supported, more on that below). Either
 start an emulator or plug-in your Android device and wait
 for it to be finished with start up processes.
@@ -23,8 +24,12 @@ Then type
 
     adb install /path/to/OfflineMapping-debug.apk
 
-After installation, and a few seconds of loading time,
-a map of Yale's campus should appear on the screen
+After installation, open the app and click "Download" to get the map
+database. Once it is finished download, click "Display" to view a map
+of Yale's campus.
+
+If you have any difficuties, email me at miller.james01@gmail.com
+with a description of the error, and I'll try to figure out what's going on.
 
 Next Steps
 ----------
@@ -38,5 +43,6 @@ accessibly by the SQLite plugin.
 Because this is a proof-of-concept, I'm not sure how
 many units there are that are easily testable, especially
 because of the brevity of the code. Nevertheless,
-writing a javascript test file that does queries on the
-database and tests file placement would be beneficial.
+dependably building a test to make sure file placement
+is handled correctly depending on the phone and platform should
+be a good gaol for the future
--- js/main.js
+++ js/main.js
@@ -6,82 +6,47 @@ function go() {
 	$('#button1').click(loadDatabase);
 }
 
-function success(entries) {
-    var i;
-    console.log(entries.length + " entries");
-    for (i=0; i<entries.length; i++) {
-    console.log(entries[i].name);
-    }
-}
-
-function fail(error) {
-    alert("Failed to list directory contents: " + error.code);
-}
-
 function onSuccess(fs) {
-    var debugReader = fs.root.createReader();
-    console.log("Reading from " + fs.root.fullPath);
-    debugReader.readEntries(success, fail)
-    window.resolveLocalFileSystemURL("cdvfile://localhost/persistent/../..",
-      function(dirEntry) {
-      	  var dr = dirEntry.createReader();
-      	  dr.readEntries(success, fail);
-      }, function() {
-      	alert("Couldn't resovle the URL")
-      });
-    fs.root.getDirectory("databases", { create: true }, function(dataEntry) {
-        var ft = new FileTransfer();
-        ft.download("https://dl.dropboxusercontent.com/s/s6pxs03krkzzvba/Database.db?dl=1&token_hash=AAEqMLcQ5aI2rjRa9kKzRHWtMYj-9shVJLncIwXi47gP3w&expiry=1399898530", // the filesystem uri you mentioned
-            "cdvfile://localhost/persistent/../../databases/Database.db", function(entry) {
-            // do what you want with the entry here
-            $('#information').html(
-                alertHtml("Successfully downloaded the file to " + entry.toURL()) 
-            );
-            $('#button1').unbind();
-            $('#button1').html("Display");
-            $('#button1').click(function () {
-                $('#map').toggle();
-                buildMap();
-            });
-        }, function(error) {
-            $('#information').html(
-              alertHtml("Error in dowloading the file from" + error.source + 
-                        "error target" + error.target + ": " + error.code)
-            );
-            console.log("error source " + error.source);
-            console.log("error target " + error.target);
-            console.log("error code " + error.code);
-        }, false, null);    
-    }, function() {
-        $('#information').html(alertHtml("Couldn't create a databases directory"));
-    });
+	fs.root.getDirectory("databases", { create: true }, function(dataEntry) {
+		var ft = new FileTransfer();
+		ft.download("https://dl.dropboxusercontent.com/s/s6pxs03krkzzvba/Database.db?dl=1&token_hash=AAEqMLcQ5aI2rjRa9kKzRHWtMYj-9shVJLncIwXi47gP3w&expiry=1399898530",
+			"cdvfile://localhost/persistent/../../databases/Database.db", function(entry) {
+			$('#information').html(
+				alertHtml("Successfully downloaded the file to " + entry.toURL()) 
+			);
+			$('#button1').unbind();
+			$('#button1').html("Display");
+			$('#button1').click(function () {
+				$('#map').toggle();
+				buildMap();
+			});
+		}, function(error) {
+			$('#information').html(
+				alertHtml("Error in dowloading the file from" + error.source + 
+					"<br>error target" + error.target + ": " + error.code)
+			);
+			console.log("error source " + error.source);
+			console.log("error target " + error.target);
+			console.log("error code " + error.code);
+		}, false, null);    
+	}, function() {
+		$('#information').html(alertHtml("Couldn't create a databases directory"));
+	});
 }
 
 function onError() {
-    $('#information').html(alertHtml("couldn't retrieve local file system"));
+	$('#information').html(alertHtml("couldn't retrieve local file system"));
 }
 
 function loadDatabase() {
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onSuccess, onError);
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onSuccess, onError);
 }
 
 function buildMap() {
 	var db = window.sqlitePlugin.openDatabase({
 		name : "Database"
 	});
-/*	
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-// add an OpenStreetMap tile layer
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// add a marker in the given location, attach some popup content to it and open the popup
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-    .openPopup();	
-*/
+	
 	var map = new L.Map('map', {
 		center : new L.LatLng(41.311000, -72.927000),
 		zoom : 14
